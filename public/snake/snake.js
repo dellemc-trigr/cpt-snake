1a55fb164456c9fa39b865c4217ac6298cdbaa4b
Thinh Nguyen <thinh.nguyen@emc.com>
2016-04-08 16:04:25 -0400

    Add screen responsiveness on mobile
    Signed-off-by: Victor Fong <victor.fong@emc.com>

--- public/snake/snake.js
+++ public/snake/snake.js
@@ -1,50 +1,46 @@
 $(document).ready(function(){
-
-	console.log('starting...');
-
-	$("#canvas").swipe( {
-    swipeStatus:function(event, phase, direction, distance)
-    {
-			if(direction == 'left' && d != "right") d = "left";
-			else if(direction == 'up' && d != "down") d = "up";
-			else if(direction == 'right' && d != "left") d = "right";
-			else if(direction == 'down' && d != "up") d = "down";
-    },
-    triggerOnTouchEnd:false,
-    threshold:200
-  });
-
-	//Canvas stuff
-	var canvas = $("#canvas")[0];
-	var ctx = canvas.getContext("2d");
-
-	var w = $("#canvas").width();
-	var h = $("#canvas").height();
-	//Lets save the cell width in a variable for easy control
-	var cw = 30;
+  var canvas = $("#snakeCanvas")
+  var ctx = canvas[0].getContext("2d")
+  var container = $(canvas).parent();
+  var cw = 20;
 	var d;
 	var food;
 	var score;
+  //Lets create the snake now
+  var snake_array; //an array of cells to make up the snake
+  drawCanvas();
 
-	//Lets create the snake now
-	var snake_array; //an array of cells to make up the snake
+  //-----------------HELPER FUNCTIONS GO HERE------------
+  function drawCanvas()
+  {
+    var width = $(container).width();
+    var height = $(container).height();
 
-	function init()
+    canvas.attr('width', width); //max width
+    canvas.attr('height', height); //max height
+
+    //Everytime this window is resized, redraw snake game
+    init(width, height);
+  }
+
+  function init(w, h)
 	{
 		d = "right"; //default direction
 		create_snake();
-		create_food(); //Now we can see the food particle
+		create_food(w, h); //Now we can see the food particle
 		//finally lets display the score
+
 		score = 0;
 
 		//Lets move the snake now using a timer which will trigger the paint function
 		//every 60ms
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
-		game_loop = setInterval(paint, 150);
+		game_loop = setInterval( function() {
+      paint(w, h);
+    }, 150);
 	}
-	init();
 
-	function create_snake()
+  function create_snake()
 	{
 		var length = 5; //Length of the snake
 		snake_array = []; //Empty array to start with
@@ -55,31 +51,28 @@ $(document).ready(function(){
 		}
 	}
 
-	//Lets create the food now
-	function create_food()
+  //Lets create the food now
+	function create_food(w, h)
 	{
 		food = {
 			x: Math.round(Math.random()*(w-cw)/cw),
 			y: Math.round(Math.random()*(h-cw)/cw),
 		};
+
 		//This will create a cell with x/y between 0-44
 		//Because there are 45(450/10) positions accross the rows and columns
 	}
 
-	//Lets paint the snake now
-	function paint()
+  //Lets paint the snake now
+	function paint(w, h)
 	{
 		//To avoid the snake trail we need to paint the BG on every frame
 		//Lets paint the canvas now
 		ctx.fillStyle = "white";
 		ctx.fillRect(0, 0, w, h);
-
-		var img=document.getElementById("scream");
-    ctx.drawImage(img,0,0, w, h);
-
+    
 		ctx.strokeStyle = "black";
 		ctx.strokeRect(0, 0, w, h);
-
 		//The movement code for the snake to come here.
 		//The logic is simple
 		//Pop out the tail cell and place it infront of the head cell
@@ -92,15 +85,15 @@ $(document).ready(function(){
 		else if(d == "left") nx--;
 		else if(d == "up") ny--;
 		else if(d == "down") ny++;
-
 		//Lets add the game over clauses now
 		//This will restart the game if the snake hits the wall
 		//Lets add the code for body collision
 		//Now if the head of the snake bumps into its body, the game will restart
-		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
+		if(nx == -1 || nx >= Math.round(w/cw) || ny == -1 || ny >= Math.round(h/cw) || check_collision(nx, ny, snake_array))
 		{
+
 			//restart game
-			init();
+			init(w, h);
 			//Lets organize the code a bit now.
 			return;
 		}
@@ -114,7 +107,7 @@ $(document).ready(function(){
 			var tail = {x: nx, y: ny};
 			score++;
 			//Create new food
-			create_food();
+			create_food(w, h);
 		}
 		else
 		{
@@ -122,7 +115,6 @@ $(document).ready(function(){
 			tail.x = nx; tail.y = ny;
 		}
 		//The snake can now eat the food.
-
 		snake_array.unshift(tail); //puts back the tail as the first cell
 
 		for(var i = 0; i < snake_array.length; i++)
@@ -139,16 +131,16 @@ $(document).ready(function(){
 		ctx.fillText(score_text, 5, h-5);
 	}
 
-	//Lets first create a generic function to paint cells
-	function paint_cell(x, y)
-	{
-		ctx.fillStyle = "blue";
-		ctx.fillRect(x*cw, y*cw, cw, cw);
-		ctx.strokeStyle = "white";
-		ctx.strokeRect(x*cw, y*cw, cw, cw);
-	}
+  //Lets first create a generic function to paint cells
+  function paint_cell(x, y)
+  {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x*cw, y*cw, cw, cw);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(x*cw, y*cw, cw, cw);
+  }
 
-	function check_collision(x, y, array)
+  function check_collision(x, y, array)
 	{
 		//This function will check if the provided x/y coordinates exist
 		//in an array of cells or not
@@ -160,7 +152,22 @@ $(document).ready(function(){
 		return false;
 	}
 
-	//Lets add the keyboard controls now
+  //-----------------EVENT REGISTER GO HERE -------------
+  $(window).resize(drawCanvas);
+
+  canvas.swipe( {
+    swipeStatus:function(event, phase, direction, distance)
+    {
+      if(direction == 'left' && d != "right") d = "left";
+      else if(direction == 'up' && d != "down") d = "up";
+      else if(direction == 'right' && d != "left") d = "right";
+      else if(direction == 'down' && d != "up") d = "down";
+    },
+    triggerOnTouchEnd:false,
+    threshold:200
+  });
+
+  //Lets add the keyboard controls now
 	$(document).keydown(function(e){
 		var key = e.which;
 		//We will add another clause to prevent reverse gear
