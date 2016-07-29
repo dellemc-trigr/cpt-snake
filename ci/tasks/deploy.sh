7506f3c2d2988eff215b8d6b41008afc71d23474
Thinh Nguyen <thinh.nguyen@emc.com>
2016-07-29 10:37:43 -0400

    Use internal docker image

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -2,13 +2,6 @@
 
 set -e -x
 
-check_param CF_IP
-check_param CF_ENDPOINT
-check_param CF_USERNAME
-check_param CF_PASSWORD
-check_param CF_ORG
-check_param CF_SPACE
-
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 export PATH=${PWD}:$PATH
 
