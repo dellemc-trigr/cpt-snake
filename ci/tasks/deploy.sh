258d77ab59b0958a715aecfc0fc6bd39bfe29298
Thinh Nguyen <thinh.nguyen@emc.com>
2016-07-29 10:04:27 -0400

    Trim down pipeline file

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -2,8 +2,6 @@
 
 set -e -x
 
-source cpt-snake/ci/tasks/util.sh
-
 check_param CF_IP
 check_param CF_ENDPOINT
 check_param CF_USERNAME
@@ -15,10 +13,6 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
 export PATH=${PWD}:$PATH
 
 cd cpt-snake/
-
-echo "${CF_IP} api.${CF_ENDPOINT}" >> /etc/hosts
-echo "${CF_IP} login.${CF_ENDPOINT}" >> /etc/hosts
-echo "${CF_IP} dojo-snake-acceptance.${CF_ENDPOINT}" >> /etc/hosts
 
 cf api "https://api.${CF_ENDPOINT}" --skip-ssl-validation
 cf auth $CF_USERNAME "$CF_PASSWORD"
